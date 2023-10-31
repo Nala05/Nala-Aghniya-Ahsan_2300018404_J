1ea94ebb918a504351ed2c0e95b3c016daa4292c
Nala Aghniya Ahsan <149520055+Nala05@users.noreply.github.com>
2023-10-31 23:58:13 +0700

    Add files via upload

---
+++ Tugas_DDP_Pertemuan 7.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+		cout<<" _  _  _\n";
+	for(int h = 1; h <= 3; h++){
+		for(int j = 3;j >= 1; j--){
+			cout<<"|_|";
+		}
+		cout<<endl;
+	}
+		
+}
+
